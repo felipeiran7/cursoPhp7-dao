c62648dac9db1a4577ee14d32f2b1d413376e4ce
Felipe Iran <felipeiran4@gmail.com>
2020-04-16 19:06:20 -0300

    adicionei funções login e busca na classe usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -52,6 +52,32 @@
 			}
 
 		}
+		public function getList(){
+			$sql= new Sql();
+			return $sql->select("SELECT *FROM tb_usuarios ORDER BY idusario");
+		}
+
+		public static function search($login){
+			$sql= new Sql();
+			return $sql->select("SELECT *FROM tb_usuarios
+				WHERE deslogin LIKE :SEARCH ORDER BY idusario", array(':SEARCH'=>"%".$login."%"));
+		}
+
+		public function login($login,$password){
+			$sql= new Sql();
+			$result= $sql->select("SELECT *FROM tb_usuarios WHERE deslogin= :LOGIN AND desenha= :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+			if(count($result)>0){
+				$row= $result[0];
+
+				$this->setIdusario($row['idusario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDesenha($row['desenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}else{
+				throw new Exception("Usuario ou senha inválidos", 1);
+				
+			}
+		}
 
 		public function __toString(){
 			return json_encode(array(
--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
 	require_once("config.php");
 
-	$usuario= new Usuario();
+	//$usuario= new Usuario();
 
-	$usuario->loadById(4);
+	//carrega 1 usuario
+	//$usuario->loadById(4);
+ 	//$lista= Usuario::getList();
+ 	//echo json_encode($lista);
 
-	echo $usuario;
+ 	//carrega uma lista de usuarios pelo login
+//search = Usuario::search("a");
+
+ 	//echo json_encode($search);
+
+ 	//carrega usuario usando login e senha
+
+ 	$usuario= new Usuario();
+ 	$usuario-> login("felipe","32322");
+
+ 	echo $usuario;
 
 ?>
